9f3988915ad25bd3fa2a87367bcbef094b372cd9
Omargazzah02 <gazzahomar2001@gmail.com>
2025-02-28 12:53:17 +0100

    commit action55

--- app/page.js
+++ app/page.js
@@ -68,7 +68,7 @@ export default function Home() {
                 value={newTask.description}
                 onChange={(e) => setNewTask({ ...newTask, description: e.target.value })}
             />
-            <button onClick={handleAddTask} className='button-normal'>Ajo!utemr c!r!c!os</button>
+            <button onClick={handleAddTask} className='button-normal'>dernier mise a jour !</button>
         </div>
     );
 }
